333e7a9aefb84496b923d5dcc48477f8c5771b5f
Katest <103419049+Ekaterlna@users.noreply.github.com>
2023-08-08 15:21:39 +0300

    init commit

---
+++ src/main/java/PosterManager.java
@@ -0,0 +1,11 @@
+public class PosterManager {
+    public Poster[] poster;
+
+    public void addPoster(Poster poster){
+        //..
+    }
+
+    public void getPosters(Poster poster){
+        //..
+    }
+}
